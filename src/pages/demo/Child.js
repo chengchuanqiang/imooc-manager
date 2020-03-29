e4fe6aefc185d270129f42daf62bfe9f87bf91e1
chengchuanqiang <1625320316@qq.com>
2020-03-29 22:57:37 +0800

    demo提交

---
+++ src/pages/demo/Child.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+class Life extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            count: 0
+        }
+    }
+
+    componentWillMount() {
+        console.log("will mount");
+    }
+
+    componentDidMount() {
+        console.log("did mount");
+    }
+
+    componentWillReceiveProps(newProps) {
+        console.log("will props" + newProps.name);
+    }
+
+    shouldComponentUpdate() {
+        console.log("should update");
+        return true;
+    }
+
+    componentWillUpdate(){
+        console.log("will update");
+    }
+
+    componentDidUpdate(){
+        console.log("did update");
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>这是子组件的生命周期</p>
+                <p>{this.props.name}</p>
+            </div>
+        )
+
+
+    }
+
+
+}
+
+export default Life;
